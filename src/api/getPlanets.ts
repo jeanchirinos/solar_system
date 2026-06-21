f872603e281ab752941b6085650c82fedafe9f8a
Jean Chrinos <jeanchirinos17@gmail.com>
2026-06-21 03:58:09 -0500

    Refactor code structure for improved readability and maintainability

--- src/api/getPlanets.ts
+++ src/api/getPlanets.ts
@@ -1,15 +1,20 @@
-import { ENV } from "@/constants/env";
-import type { PlanetsInfo } from "@/types/Planet";
+// import { ENV } from "@/constants/env";
+// import type { PlanetsInfo } from "@/types/Planet";
+import planets from "@/data/planets.json";
+
+// export async function getPlanets() {
+//   const response = await fetch(
+//     `${ENV.API_URL}/api/planets`,
+//     {
+//       cache: "force-cache",
+//     },
+//   );
+
+//   const data = await response.json();
+
+//   return data as PlanetsInfo;
+// }
 
 export async function getPlanets() {
-  const response = await fetch(
-    `${ENV.API_URL}/api/planets`,
-    {
-      cache: "force-cache",
-    },
-  );
-
-  const data = await response.json();
-
-  return data as PlanetsInfo;
+  return planets;
 }
--- src/api/getSatellites.ts
+++ src/api/getSatellites.ts
@@ -1,15 +1,20 @@
-import { ENV } from "@/constants/env";
-import type { SatellitesInfo } from "@/types/Satellite";
+// import { ENV } from "@/constants/env";
+// import type { SatellitesInfo } from "@/types/Satellite";
+import satellites from "@/data/satellites.json";
+
+// export async function getSatellites() {
+//   const response = await fetch(
+//     `${ENV.API_URL}/api/satellites`,
+//     {
+//       cache: "force-cache",
+//     },
+//   );
+
+//   const data = await response.json();
+
+//   return data as SatellitesInfo;
+// }
 
 export async function getSatellites() {
-  const response = await fetch(
-    `${ENV.API_URL}/api/satellites`,
-    {
-      cache: "force-cache",
-    },
-  );
-
-  const data = await response.json();
-
-  return data as SatellitesInfo;
+  return satellites;
 }
